dec493306c38178c3a5a2624f16f3d15ce1c0803
mQfZ <56398396+mQfZ@users.noreply.github.com>
2023-07-06 02:01:14 -0400

    Added zsh plugins

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -4,3 +4,4 @@ source $ZDOTDIR/config.zsh
 source $ZDOTDIR/color.zsh
 source $ZDOTDIR/prompt.zsh
 source $ZDOTDIR/extensions.zsh
+source $ZDOTDIR/plugin.zsh
---
+++ zsh/plugin.zsh
@@ -0,0 +1,2 @@
+source $ZDOTDIR/plugin/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $ZDOTDIR/plugin/zsh-autosuggestions/zsh-autosuggestions.zsh
